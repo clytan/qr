46f8a1872eeace2c55048cffb607614ca7516141
clytan <clytandsilva97@gmail.com>
2025-08-27 04:07:40 +0530

    user init

---
+++ user/src/components/csslinks.php
@@ -0,0 +1,14 @@
+<link id="bootstrap" href="../assets/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link id="bootstrap-grid" href="../assets/bootstrap-grid.min.css" rel="stylesheet" type="text/css" />
+<link id="bootstrap-reboot" href="../assets/bootstrap-reboot.min.css" rel="stylesheet" type="text/css" />
+<link href="../assets/animate.css" rel="stylesheet" type="text/css" />
+<link href="../assets/owl.carousel.css" rel="stylesheet" type="text/css" />
+<link href="../assets/owl.theme.css" rel="stylesheet" type="text/css" />
+<link href="../assets/owl.transitions.css" rel="stylesheet" type="text/css" />
+<link href="../assets/magnific-popup.css" rel="stylesheet" type="text/css" />
+<link href="../assets/jquery.countdown.css" rel="stylesheet" type="text/css" />
+<link href="../assets/style.css" rel="stylesheet" type="text/css" />
+<link href="../assets/de-grey.css" rel="stylesheet" type="text/css" />
+<!-- color scheme -->
+<link id="colors" href="../assets/colors/scheme-04.css" rel="stylesheet" type="text/css" />
+<link href="../assets/coloring.css" rel="stylesheet" type="text/css" />
